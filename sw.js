ec524548a4d1ec56da5a68425cdea07fdd258a2e
Darag93 <daragwolf93@gmail.com>
2026-04-12 17:32:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    // Změna názvu na vlk-v2 vynutí aktualizaci u všech uživatelů
-    caches.open('bio-vlk-v2').then((cache) => {
+    // Změna na v3 - tohle je ten příkaz, který vymaže raketu
+    caches.open('bio-vlk-v3').then((cache) => {
       return cache.addAll([
         'bio.html',
         'manifest.json',
@@ -13,12 +13,13 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (event) => {
-  // Tento kód smaže starou mezipaměť s raketou (bio-v1)
+  // Smaže úplně všechno staré (v1 i v2 s raketou)
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== 'bio-vlk-v2') {
+          if (cacheName !== 'bio-vlk-v3') {
+            console.log('Mažu starou mezipaměť:', cacheName);
             return caches.delete(cacheName);
           }
         })
