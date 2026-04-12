280390fb8f33788a09b6c22422c519c381662836
Darag93 <daragwolf93@gmail.com>
2026-04-12 17:29:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('bio-vlk-v2').then((cache) => cache.addAll(['bio.html', 'manifest.json']))
+    // Změna názvu na vlk-v2 vynutí aktualizaci u všech uživatelů
+    caches.open('bio-vlk-v2').then((cache) => {
+      return cache.addAll([
+        'bio.html',
+        'manifest.json',
+        'index.html',
+        'styl.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Tento kód smaže starou mezipaměť s raketou (bio-v1)
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'bio-vlk-v2') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
