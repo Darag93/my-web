e830816c38f19dbdcc4e2bc9bb0a8eb1d8bbc884
Darag93 <daragwolf93@gmail.com>
2026-04-12 14:19:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-const cacheName = 'bioflow-v1';
-const assets = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            cache.addAll(assets);
-        })
-    );
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('bio-v1').then((cache) => cache.addAll(['bio.html', 'manifest.json']))
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
