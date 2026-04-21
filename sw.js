88d46024162293ea2fc3bdf7dc813ff36cf20e96
Darag93 <daragwolf93@gmail.com>
2026-04-21 23:09:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,10 @@
-const cacheName = 'bio-vlk-v3-v2';
-const filesToCache = [
-  './bio-vlk-v3.html',
-  './manifest.json',
-  './index.html'
-];
+const cacheName = 'bio-vlk-v3-final';
+const filesToCache = ['./bio-vlk-v3.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== cacheName) return caches.delete(key);
-      }));
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(filesToCache)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
