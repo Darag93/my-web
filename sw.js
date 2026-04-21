350ca9a6e1e046156546f8f28a037dffe7452337
Darag93 <daragwolf93@gmail.com>
2026-04-21 23:23:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-const cacheName = 'bio-vlk-v3-final';
-const filesToCache = ['./bio-vlk-v3.html', './manifest.json'];
+const cacheName = 'bio-vlk-v3-final-v2';
+const filesToCache = [
+  './bio-vlk-v3.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2829/2829824.png'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(filesToCache)));
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
