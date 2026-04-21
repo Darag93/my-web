4b90bde357374eb6ca37e95c57895689074c28f0
Darag93 <daragwolf93@gmail.com>
2026-04-21 22:29:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-const cacheName = 'bio-vlk-v3-cache';
+const cacheName = 'bio-vlk-v3-v1';
 const filesToCache = [
-  'bio-vlk-v3.html',
-  'manifest.json'
+  './bio-vlk-v3.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== cacheName) return caches.delete(key);
+      }));
+    })
   );
 });
 
