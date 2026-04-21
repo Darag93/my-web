bb43dd336d06cecca92277b6f3cbf50735f18f6d
Darag93 <daragwolf93@gmail.com>
2026-04-21 22:25:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,32 +5,13 @@ const filesToCache = [
 ];
 
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
